33d3cb8ee4cc07246b777396b939fcbaa7fbe970
evilrise7 <bakha898@mail.ru>
2019-01-24 22:32:09 +0600

    Первая типа альфа версия

---
+++ yandexpygame.py
@@ -0,0 +1,613 @@
+import pygame as pg
+import os
+import sys
+import random
+import datetime
+
+
+pg.init()
+pg.display.set_caption("Hell Obtained Sensible Tiny And Geniusly Emmy.")
+pg.display.set_icon(pg.image.load("sprites\icon.png"))
+WIDTH = 640
+HEIGHT = 512
+screen = pg.display.set_mode((WIDTH, HEIGHT))
+pg.mouse.set_visible(False)
+pg.key.set_repeat(500, 100)
+
+all_sprites = pg.sprite.Group()
+tiles_group = pg.sprite.Group()
+entities_group = pg.sprite.Group()
+inventory_group = pg.sprite.Group()
+drop_group = pg.sprite.Group()
+animal_group = pg.sprite.Group()
+
+
+def load_image(name, colorkey=None):
+    fullname = os.path.join('sprites', name)
+    try:
+        image = pg.image.load(fullname)
+    except pg.error as message:
+        print('Хьюстон, у нас проблемы с ', name)
+        raise SystemExit(message)
+    image = image.convert_alpha()
+    if colorkey is not None:
+        if colorkey is -1:
+            colorkey = image.get_at((0, 0))
+        image.set_colorkey(colorkey)
+    return image
+
+
+tile_images = {"grass": load_image('grass.png'),
+               "sand": load_image('sand.png'),
+               "stone": load_image('stone.png'),
+               "empty": load_image('empty_tile.png')}
+
+entity_images = {"green": load_image('green.png'),
+                 "flowers": load_image('flowers.png'),
+                 "rock": load_image('rock.png'),
+                 "bush": load_image('bush.png')}
+
+inventory_images = {"empty": load_image('empty.png'),
+                    "meat_block": load_image('meat_block_inv.png'),
+                    "eyes": load_image('eyes_inv.png'),
+                    "meat": load_image('meat_inv.png'),
+                    "gold": load_image('gold_inv.png'),
+                    "gold_sword": load_image('gold_sword_inv.png'),
+                    "silver_sword": load_image('silver_sword_inv.png')}
+
+drop_images = {"meat": load_image('meat.png')}
+
+
+class Game:  # Инициализация игры
+    def __init__(self):
+        self.dt = 0
+        self.FPS = 60
+        self.clock = pg.time.Clock()
+        self.timer_cut = 0
+        self.time_in_game = 0
+        self.mousepos = (0, 0)
+
+        self.step = 1
+
+        self.world = TileMap()
+        self.inventory = Inventory()
+        posx = 0
+        posy = 0
+
+        quarter = random.randint(1, 4)
+
+        if quarter == 1:
+            posx = random.randint(1, 15)
+            posy = random.randint(1, 15)
+
+        if quarter == 2:
+            posx = random.randint(16, 31)
+            posy = random.randint(16, 31)
+
+        if quarter == 3:
+            posx = random.randint(32, 47)
+            posy = random.randint(32, 47)
+
+        if quarter == 4:
+            posx = random.randint(48, 63)
+            posy = random.randint(48, 63)
+
+        self.player = Player(all_sprites, self, posx, posy, 64)
+        self.camera = Camera()
+
+        self.mobs = []
+        for _ in range(random.randint(24, 32)):
+            self.mobs.append(Cow(animal_group,
+                                 self, random.randint(2, 62), random.randint(2, 62), 64))
+
+    def player_run(self):
+        self.player.state = "run"
+        self.player.timer = 0.05
+        self.gender()
+
+    def gender(self):
+        if self.player.gender == "male":
+            self.player.cut_sheet(load_image("p_sheet.png"))
+        else:
+            self.player.cut_sheet(load_image("f_sheet.png"))
+
+    def player_stay(self):
+        self.player.state = "stay"
+        self.player.timer = 0.5
+        self.gender()
+
+    def update_tiles(self):
+        for sprite in drop_group:
+            screen.blit(sprite.image, self.camera.apply(sprite))
+        for sprite in tiles_group:
+            screen.blit(sprite.image, self.camera.apply(sprite))
+        for sprite in entities_group:
+            screen.blit(sprite.image, self.camera.apply(sprite))
+        for sprite in animal_group:
+            screen.blit(sprite.image, self.camera.apply(sprite))
+        for sprite in all_sprites:
+            screen.blit(sprite.image, self.camera.apply(sprite))
+
+    def world_cutting(self):
+        self.timer_cut += self.dt
+        if self.timer_cut > 1:
+            tiles_group.empty()  # очистка всех спрайтов, чтобы не нагружать память
+            self.step += 1
+
+            for i in range(self.world.w):
+                self.world.board[self.step - 1][i] = -1
+                self.world.board[-self.step][i] = -1
+
+            for i in range(self.world.h):
+                self.world.board[i][self.step - 1] = -1
+                self.world.board[i][-self.step] = -1
+
+            self.world.render()
+
+            tiles_group.update()
+            entities_group.update()
+
+            self.timer_cut = 0
+
+    def run(self):
+        self.world.render()
+        self.inventory.render()
+        self.running = True
+
+        tiles_group.update()
+        all_sprites.update()
+        animal_group.update()
+        inventory_group.update()
+        entities_group.update()
+
+        cursor = load_image("cursor.png")
+        cursor = pg.transform.scale(cursor, (64, 64))
+
+        while self.running:
+            screen.fill((0, 0, 0))
+            self.dt = self.clock.tick(self.FPS) / 1000
+
+            self.time_in_game += self.dt
+            self.world_cutting()
+            self.camera.update(self.player)
+            self.update_tiles()
+
+            for event in pg.event.get():
+                if event.type == pg.QUIT:
+                    self.running = False
+
+                if event.type == pg.MOUSEMOTION:
+                    if pg.mouse.get_focused():
+                        self.mousepos = event.pos
+
+                if event.type == pg.KEYDOWN:
+                    if event.key == pg.K_ESCAPE:
+                        self.quit()
+
+                    if event.key == pg.K_a or event.key == pg.K_w \
+                            or event.key == pg.K_s or event.key == pg.K_d:
+                        self.player_run()
+
+                if event.type == pg.KEYUP:
+                    if event.key == pg.K_a or event.key == pg.K_w \
+                            or event.key == pg.K_s or event.key == pg.K_d:
+                        self.player_stay()
+            animal_group.update()
+            all_sprites.update()
+
+            inventory_group.draw(screen)
+
+            cursorx = int(self.mousepos[0])
+            cursory = int(self.mousepos[1])
+
+            screen.blit(cursor, (cursorx, cursory))
+
+            pg.display.flip()
+
+        pg.quit()
+        print(str(datetime.timedelta(seconds=round(self.time_in_game, 2))))
+
+    def quit(self):
+        pg.quit()
+        sys.exit()
+
+
+class TileMap:
+    def __init__(self):
+        self.w = 66
+        self.h = 66
+        self.board = [[0] * self.w for _ in range(self.h)]
+        self.entities = [[0] * self.w for _ in range(self.h)]
+        self.generation()
+        self.cell_size = 64
+        self.entities_enabled = False
+
+    def set_view(self, cell_size):
+        self.cell_size = cell_size
+
+    def generation(self):
+        for j in range(self.h):
+            for i in range(self.w):
+                b = random.randint(0, 100)
+                if b < 65:
+                    self.board[j][i] = 0
+                elif 65 <= b < 80:
+                    self.board[j][i] = 1
+                else:
+                    self.board[j][i] = 2
+        for i in range(self.w):
+            self.board[0][i] = -1
+            self.board[-1][i] = -1
+        for i in range(self.h):
+            self.board[i][0] = -1
+            self.board[i][-1] = -1
+
+    def render(self):
+        for i in range(self.w):
+            for j in range(self.h):
+                if self.board[j][i] == -1:
+                    Tile('empty', i, j)
+
+                    if self.entities[j][i] != 0:
+                        self.entities[j][i].get_out(Tile('empty', i, j))
+                        self.entities[j][i] = 0
+
+                if self.board[j][i] == 0:
+                    Tile('grass', i, j)
+                    if not self.entities_enabled:
+                        b = random.randint(0, 100)
+                        if b < 40:
+                            self.entities[j][i] = Entity('green', i, j)
+                        elif 45 > b >= 40:
+                            self.entities[j][i] = Entity('flowers', i, j)
+                        elif 60 > b >= 50:
+                            self.entities[j][i] = Entity('bush', i, j)
+
+                if self.board[j][i] == 1:
+                    Tile('sand', i, j)
+                    if not self.entities_enabled:
+                        b = random.randint(0, 100)
+                        if b < 4:
+                            self.entities[j][i] = Entity('rock', i, j)
+
+                if self.board[j][i] == 2:
+                    Tile('stone', i, j)
+                    if not self.entities_enabled:
+                        b = random.randint(0, 100)
+                        if b < 11:
+                            self.entities[j][i] = Entity('rock', i, j)
+        self.entities_enabled = True
+
+    def get_click(self, mouse_pos):
+        cell = self.get_tile(mouse_pos)
+        self.tile_click(cell)
+
+    def get_tile(self, mouse_pos):
+        x = mouse_pos[0] // self.cell_size
+        y = mouse_pos[1] // self.cell_size
+        return x, y
+
+    def tile_click(self, cell):
+        if self.board[cell[1]][cell[0]] < 1:
+            self.board[cell[1]][cell[0]] += 1
+        else:
+            self.board[cell[1]][cell[0]] = 0
+
+
+class Camera:
+    # зададим начальный сдвиг камеры
+    def __init__(self):
+        self.dx = 0
+        self.dy = 0
+        self.camera = pg.Rect(0, 0, 4224, 4224)
+
+        self.width = 4224
+        self.height = 4224
+
+    # сдвинуть объект obj на смещение камеры
+    def apply(self, obj):
+        return obj.rect.move(self.camera.topleft)
+
+    def update(self, target):
+        x = -target.rect.x - target.rect.w + int(640 // 2)
+        y = -target.rect.y - target.rect.h + int(512 // 2)
+
+        x = min(0, x)  # ограничение на левый край
+        y = min(0, y)  # ограничение на верхний край
+        x = max(-(self.width - 640), x)  # на правый край
+        y = max(-(self.height - 512), y)  # на нижний
+
+        self.camera = pg.Rect(x, y, self.width, self.height)
+
+
+class Tile(pg.sprite.Sprite):
+    def __init__(self, type, x, y):
+        super().__init__(tiles_group)
+        self.cell_size = 64
+        self.image = tile_images[type]
+        self.image = pg.transform.scale(self.image,
+                                        (self.cell_size, self.cell_size))
+        self.rect = self.image.get_rect().move(self.cell_size * x,
+                                               self.cell_size * y)
+
+
+class Entity(pg.sprite.Sprite):
+    def __init__(self, type, x, y):
+        super().__init__(entities_group)
+        self.cell_size = 64
+        self.image = entity_images[type]
+        self.image = pg.transform.scale(self.image,
+                                        (self.cell_size, self.cell_size))
+        self.rect = self.image.get_rect().move(self.cell_size * x,
+                                               self.cell_size * y)
+
+    def get_event(self, event):
+        if self.rect.collidepoint(event.pos):
+            self.kill()
+            return 1
+
+    def get_out(self, tile):
+        if self.rect.collidepoint(tile.rect.topleft):
+            self.kill()
+
+
+class Player(pg.sprite.Sprite):
+    def __init__(self, group, game, x, y, cell_size):
+        super().__init__(group)
+        self.game = game
+        self.state = "stay"
+        self.tmpstate = False
+        self.frames = []
+        self.cur_frame = 0
+
+        self.gender = "male"
+
+        if self.gender == "male":
+            self.cut_sheet(load_image("p_sheet.png"))
+        else:
+            self.cut_sheet(load_image("f_sheet.png"))
+
+        self.image = self.frames[self.cur_frame]
+        self.size = self.image.get_size()
+
+        self.rect = self.image.get_rect()
+        self.cell_size = cell_size
+
+        self.speed = 150
+
+        self.vx = 0
+        self.vy = 0
+
+        self.timer = 0
+        self.x = x * cell_size
+        self.y = y * cell_size
+
+    def comparestates(self):
+        if self.tmpstate != self.state:
+            self.frames.clear()
+            self.tmpstate = self.state
+
+    def cut_sheet(self, sheet):
+        self.comparestates()
+        self.rect = pg.Rect(0, 0, sheet.get_width() // 6,
+                            sheet.get_height() // 2)
+
+        if self.state == "run":
+            for j in range(1, 2):
+                for i in range(6):
+                    frame_location = (self.rect.w * i, self.rect.h * j)
+                    self.frames.append(sheet.subsurface(pg.Rect(
+                        frame_location, self.rect.size)))
+
+            self.frames = self.frames[0:6]
+
+        if self.state == "stay":
+            for j in range(0, 1):
+                for i in range(2):
+                    frame_location = (self.rect.w * i, self.rect.h * j)
+                    self.frames.append(sheet.subsurface(pg.Rect(
+                        frame_location, self.rect.size)))
+
+            self.frames = self.frames[0:2]
+
+    def key_movement(self):
+        self.vx = 0
+        self.vy = 0
+        keys = pg.key.get_pressed()
+
+        if keys[pg.K_a]:
+            self.vx = -self.speed
+        if keys[pg.K_d]:
+            self.vx = self.speed
+        if keys[pg.K_w]:
+            self.vy = -self.speed
+        if keys[pg.K_s]:
+            self.vy = self.speed
+
+        if self.vx == self.vy == 0:
+            self.state = "stay"
+        else:
+            self.state = "run"
+
+    def update(self):
+        self.timer += self.game.dt
+        if self.state == "stay":
+            if self.timer > 0.5:
+                self.cur_frame = (self.cur_frame + 1) % len(self.frames)
+                self.image = self.frames[self.cur_frame]
+                self.timer = 0
+
+        if self.state == "run":
+            if self.timer > 0.05:
+                self.cur_frame = (self.cur_frame + 1) % len(self.frames)
+                self.image = self.frames[self.cur_frame]
+                self.timer = 0
+
+        self.image = pg.transform.scale(self.image,
+                                        (int(self.size[0] * 4),
+                                         int(self.size[1] * 4)))
+        self.key_movement()
+
+        self.x += self.vx * self.game.dt
+        self.y += self.vy * self.game.dt
+        self.rect.x = self.x
+        self.rect.y = self.y
+
+
+class Inventory:
+    def __init__(self):
+        self.w = 3
+        self.inv = [[0] * self.w for _ in range(2)]
+
+    def render(self):
+        for i in range(self.w):
+            for j in range(1):
+                if self.inv[j][i] == 0:
+                    Inventory_Tile('gold', i)
+
+
+class Inventory_Tile(pg.sprite.Sprite):
+    def __init__(self, type, x):
+        super().__init__(inventory_group)
+        self.cell_size = 72
+        self.image = inventory_images[type]
+        self.image = pg.transform.scale(self.image,
+                                        (self.cell_size, self.cell_size))
+        self.rect = self.image.get_rect().move(self.cell_size * x + 360, 0)
+
+
+class Drop(pg.sprite.Sprite):
+    def __init__(self, type, player, x, y):
+        super().__init__(drop_group)
+        self.type = type
+        self.cell_size = 64
+        self.image = drop_images[type]
+        self.image = pg.transform.scale(self.image,
+                                        (self.cell_size, self.cell_size))
+
+        self.rect = self.image.get_rect().move(self.cell_size * x,
+                                               self.cell_size * y)
+        self.player = player
+
+    def get_event(self, event):
+        if self.rect.collidepoint((self.player.rect.x, self.player.rect.y)):
+            self.kill()
+
+
+class Cow(pg.sprite.Sprite):
+    def __init__(self, group, game, x, y, cell_size):
+        super().__init__(group)
+        self.game = game
+        self.state = "stay"
+        self.tmpstate = False
+
+        self.frames = []
+        self.cur_frame = 0
+
+        self.cut_sheet(load_image("c_sheet.png"))
+
+        self.image = self.frames[self.cur_frame]
+        self.size = self.image.get_size()
+
+        self.rect = self.image.get_rect()
+        self.cell_size = cell_size
+
+        self.speed = 100
+
+        self.vx = 0
+        self.vy = 0
+
+        self.timer = 0
+        self.timer_choose_animation = 0
+
+        self.x = x * cell_size
+        self.y = y * cell_size
+
+        self.movement(random.randint(0, 4))
+
+    def comparestates(self):
+        if self.tmpstate != self.state:
+            self.frames.clear()
+            self.tmpstate = self.state
+
+    def cut_sheet(self, sheet):
+        self.comparestates()
+
+        self.rect = pg.Rect(0, 0, sheet.get_width() // 2,
+                            sheet.get_height() // 2)
+
+        if self.state == "run":
+            for j in range(1, 2):
+                for i in range(2):
+                    frame_location = (self.rect.w * i, self.rect.h * j)
+                    self.frames.append(sheet.subsurface(pg.Rect(frame_location,
+                                                                self.rect.size)))
+
+        if self.state == "stay":
+            for j in range(0, 1):
+                for i in range(2):
+                    frame_location = (self.rect.w * i, self.rect.h * j)
+                    self.frames.append(sheet.subsurface(pg.Rect(frame_location,
+                                                                self.rect.size)))
+
+        self.frames = self.frames[0:2]
+
+    def movement(self, b):
+        self.vx = 0
+        self.vy = 0
+
+        if b == 0:
+            self.vx = 0
+            self.vy = 0
+            self.state = "stay"
+            self.cut_sheet(load_image("c_sheet.png"))
+
+        if b == 1:
+            self.vx = -self.speed
+
+        if b == 2:
+            self.vx = self.speed
+
+        if b == 3:
+            self.vy = -self.speed
+
+        if b == 4:
+            self.vy = self.speed
+
+        if b > 0:
+            self.state = "run"
+            self.cut_sheet(load_image("c_sheet.png"))
+
+    def update(self):
+        self.timer += self.game.dt
+        self.timer_choose_animation += self.game.dt
+
+        if self.state == "stay":
+            if self.timer > 1:
+                self.cur_frame = (self.cur_frame + 1) % len(self.frames)
+                self.image = self.frames[self.cur_frame]
+                self.timer = 0
+
+        if self.state == "run":
+            if self.timer > 0.3:
+                self.cur_frame = (self.cur_frame + 1) % len(self.frames)
+                self.image = self.frames[self.cur_frame]
+                self.timer = 0
+
+        self.image = pg.transform.scale(self.image,
+                                        (int(self.size[0] * 4),
+                                         int(self.size[1] * 4)))
+
+        if self.timer_choose_animation > 2:
+            self.timer_choose_animation = 0
+            self.movement(random.randint(0, 4))
+
+        self.x += self.vx * self.game.dt
+        self.y += self.vy * self.game.dt
+        self.rect.x = self.x
+        self.rect.y = self.y
+
+
+# Создание/Отрисовка мира
+world = Game()
+world.run()
